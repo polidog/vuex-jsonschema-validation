5518160470e01b611c981315ec4824636a1276f8
polidog <polidogs@gmail.com>
2019-11-10 22:44:43 +0900

    Fix data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import Ajv from 'ajv'
+import deepcopy from 'deepcopy'
 const ajv = new Ajv({allErrors: true})
 
 export default function storeJsonSchemaValidation(schema) {
-  return store => {    
+  return store => {
+    const prevState = deepcopy(store.state)
     store.subscribe((_, state) => {
       if (!ajv.validate(schema, state)) {
+        store.replaceState(prevState)
         throw new Error(ajv.errorsText())
       }
     })
